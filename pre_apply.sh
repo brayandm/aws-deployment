317eb01ac382a42cc134cd7635961d54b8af43f6
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-09-07 06:22:48 +0200

    updatint scripts

--- pre_apply.sh
+++ pre_apply.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#Remove the old ssh key
+# Remove the old ssh key
 rm -f ./ssh/terraform*
 
-#Create a new ssh key
+# Create a new ssh key
 ssh-keygen -t ed25519 -f ./ssh/terraform -N "" > /dev/null || true
 
 
