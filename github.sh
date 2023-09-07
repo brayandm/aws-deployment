b60ec8c254bb7e7960388a851ec47d15784de6a0
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-09-07 09:00:40 +0200

    github added

--- github.sh
+++ github.sh
@@ -9,12 +9,22 @@ if [ -z "${GITHUB_REPOS}" ]; then
 fi
 
 IFS=',' read -ra ADDR <<< "${GITHUB_REPOS}"
-for i in "${ADDR[@]}"; do
-    curl -L \
+for GITHUB_URL in "${ADDR[@]}"; do
+    export GITHUB_RUNNER_TOKEN=$(curl -L \
         -X POST \
         -H "Accept: application/vnd.github+json" \
         -H "Authorization: Bearer ${GITHUB_TOKEN}" \
         -H "X-GitHub-Api-Version: 2022-11-28" \
-        https://api.github.com/repos/$i/actions/runners/registration-token \
-            | jq -r .token
+        https://api.github.com/repos/$GITHUB_URL/actions/runners/registration-token \
+            | jq -r .token)
+
+        echo -e "\n\n#Downloading and Configuring the github runner" >> ./prepare_server_with_envs.sh
+        echo -e "su - github-runner -c \"mkdir -p $GITHUB_URL\"" >> ./prepare_server_with_envs.sh
+        echo -e "su - github-runner -c \"cd $GITHUB_URL && curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz\"" >> ./prepare_server_with_envs.sh
+        echo -e "su - github-runner -c \"cd $GITHUB_URL && echo \"292e8770bdeafca135c2c06cd5426f9dda49a775568f45fcc25cc2b576afc12f  actions-runner-linux-x64-2.304.0.tar.gz\" | shasum -a 256 -c\"" >> ./prepare_server_with_envs.sh
+        echo -e "su - github-runner -c \"cd $GITHUB_URL && tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz\"" >> ./prepare_server_with_envs.sh
+        echo -e "su - github-runner -c \"cd $GITHUB_URL && ./config.sh --url https://github.com/$GITHUB_URL --token $GITHUB_RUNNER_TOKEN --unattended\"" >> ./prepare_server_with_envs.sh
+        echo -e "cd /home/github-runner/$GITHUB_URL" >> ./prepare_server_with_envs.sh
+        echo -e "./svc.sh install github-runner" >> ./prepare_server_with_envs.sh
+        echo -e "sudo ./svc.sh start" >> ./prepare_server_with_envs.sh
 done
--- post_apply.sh
+++ post_apply.sh
@@ -3,14 +3,23 @@
 # Sleep for 10 seconds to allow the server to boot
 sleep 10
 
-#Copying the script to the server
+# Export the public ip of the ec2 instance
+export EC2_PUBLIC_IP=$(terraform output --raw ec2_public_ip)
+
+# Substitute the variables in the prepare_server.sh file
+envsubst "$(env | grep -v '^VERSION_CODENAME=' | sed -e 's/=.*//' -e 's/^/\$/g')" < prepare_server.sh > prepare_server_with_envs.sh
+
+# Execute github.sh
+bash github.sh
+
+# Copying the script to the server
 scp -i ./ssh/terraform -o StrictHostKeyChecking=no prepare_server_with_envs.sh ubuntu@$EC2_PUBLIC_IP:~
 
-#Moving the script on the server to root
+# Moving the script on the server to root
 ssh -i ./ssh/terraform -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP "echo 'mv /home/ubuntu/prepare_server_with_envs.sh /root/' | sudo -i"
 
-#Running the script on the server
+# Running the script on the server
 ssh -i ./ssh/terraform -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP "echo 'bash prepare_server_with_envs.sh' | sudo -i"
 
-#Deleting the script from the server
+# Deleting the script from the server
 ssh -i ./ssh/terraform -o StrictHostKeyChecking=no ubuntu@$EC2_PUBLIC_IP "echo 'rm prepare_server_with_envs.sh' | sudo -i"
