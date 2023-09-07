3f5254946f27c0ab0c9d17fa5ff1f61e13e70fb0
Brayan Durán Medina <brayanduranmedina@gmail.com>
2023-09-07 11:50:14 +0200

    update

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_key_pair" "ssh_key" {
 
 resource "aws_instance" "ec2" {
   ami           = "ami-053b0d53c279acc90"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
   key_name      = aws_key_pair.ssh_key.key_name
 
   vpc_security_group_ids      = [aws_security_group.security_group.id]
